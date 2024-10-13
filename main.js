b8a66b2eeb1597c3c62110c85c8d49ac72da3477
Keith <keith.githinji@student.moringaschool.com>
2024-10-13 12:37:22 +0300

    Completed the lab

--- main.js
+++ main.js
@@ -4,8 +4,49 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const EMPTY_HEART = '♡';
+  const FULL_HEART = '♥';
 
+  const modal = document.getElementById('modal');
+  const modalMessage = document.getElementById('modal-message');
 
+  modal.classList.add('hidden');
+
+  const likeGlyphs = document.querySelectorAll('.like-glyph');
+
+  likeGlyphs.forEach(likeGlyph => {
+    likeGlyph.addEventListener('click', () => {
+    
+      if (likeGlyph.innerText === FULL_HEART) {
+  
+        likeGlyph.innerText = EMPTY_HEART;
+        likeGlyph.classList.remove('activated-heart');
+      } else {
+      
+        mimicServerCall()
+          .then(() => {
+        
+            likeGlyph.innerText = FULL_HEART;
+            likeGlyph.classList.add('activated-heart'); 
+
+            
+            modal.classList.add('hidden');
+          })
+          .catch((error) => {
+            
+            modal.classList.remove('hidden');
+            modalMessage.innerText = error; 
+
+            
+            setTimeout(() => {
+              modal.classList.add('hidden');
+            }, 3000);
+          });
+      }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
